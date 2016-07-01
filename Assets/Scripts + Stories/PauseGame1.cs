7e6deb198634bc1d2c39b9eca2f4324d55e2199c
KajG <kaaiman99@hotmail.com>
2016-07-01 15:05:15 +0200

    De game

---
+++ Assets/Scripts + Stories/PauseGame1.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseGame1 : MonoBehaviour {
+
+    public Transform canvas;
+    public Transform player;
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Pause();
+        }
+    }
+
+    public void Pause()
+    {
+        if (canvas.gameObject.activeInHierarchy == false)
+        {
+            canvas.gameObject.SetActive(true);
+            Time.timeScale = 0;
+            player.GetComponent<PlayerShooting>().enabled = false;
+            player.GetComponent<Playermovement>().enabled = false;
+        }
+        else
+        {
+            canvas.gameObject.SetActive(false);
+            Time.timeScale = 1;
+            player.GetComponent<PlayerShooting>().enabled = true;
+            player.GetComponent<Playermovement>().enabled = true;
+        }
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
